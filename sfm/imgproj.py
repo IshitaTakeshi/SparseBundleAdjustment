15c4dd3461be1c8e65c141b6566ac492a6a8e4dd
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-11-07 15:46:19 +0900

    Add a camera projection function

--- sfm/imgproj.py
+++ sfm/imgproj.py
@@ -1,3 +1,20 @@
+def camera_projection(a, X):
+    fx = a[0]
+    fy = a[0] * a[3]
+    s = a[4]
+    c = a[1:3]  # offset
+
+    A = np.array([
+        [fx, s, 0],
+        [0, fy, 0],
+        [0,  0, 1]
+    ])
+
+    Z = np.dot(A, X)
+    n = Z[0:2] / Z[2] + c
+    return n
+
+
 def calcImgProj(a[5], q[4], v[3], t[3], m[3]):  # q -> qr0, m -> M
     L = sqrt(1.0 - np.dot(v, v));
 
@@ -27,14 +44,8 @@ def calcImgProj(a[5], q[4], v[3], t[3], m[3]):  # q -> qr0, m -> M
 
     u = np.dot(-P, r) + t
 
-    fx, fy = a[0], a[0] * a[3]
-    cx, cy = a[1], a[2]
-    s = a[4]
+    return camera_projection(a, u)
 
-    n[0] = (fx * u[0] + s * u[1]) / u[2] + cx
-    n[1] = (fy * u[1]) / u[2] + cy
-
-    return n
 
 def calc_image_projection(a, qr0, v, t, m):
     A = qr0[0]
@@ -46,9 +57,7 @@ def calc_image_projection(a, qr0, v, t, m):
     p = D * m + np.cross(r, m)
     u = np.cross(r, p) + D * p + np.dot(r, m) * r + t
 
-    n[0] = (a[0] * u[0] + a[4] * u[1]) / u[2] + a[1]
-    n[1] = (a[0] * a[3] * u[1]) / u[2]        + a[2]
-
+    return camera_projection(a, u)
 
 
 def calcImgProjFullR(a[5], qr0[4], t[3], M[3], n[2]):
@@ -65,8 +74,5 @@ def calcImgProjFullR(a[5], qr0[4], t[3], M[3], n[2]):
         [-q[3], -q[2], +q[1], +q[0]]
     ])
 
-    X = np.array(ql, np.dot(qr, M)) + t
-
-    n[0] = (a[0]*X[0] + a[4]*X[1] + a[1]*X[2]) / X[2]
-    n[1] = (a[0]*a[3]*X[1] + a[2]*X[2]) / X[2]
-    return n
+    u = np.array(ql, np.dot(qr, M)) + t
+    return camera_projection(a, u)
