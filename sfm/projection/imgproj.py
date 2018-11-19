bc6cc73656e15903b2828fc84aedbf97a4a19209
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-11-19 17:11:09 +0900

    Cleanup projection

--- sfm/projection/imgproj.py
+++ sfm/projection/imgproj.py
@@ -21,8 +21,6 @@ def right_matrix(q):
     return Q
 
 
-
-
 def projection(camera_parameters, initial_rotation, pose, points3d):
     return projection_(
         camera_parameters,
@@ -33,50 +31,7 @@ def projection(camera_parameters, initial_rotation, pose, points3d):
     )
 
 
-def projection_(a, q, v, t, m):
-    w = sqrt(1.0-np.dot(v, v))
-    v = np.concatenate(([w], v))
-    m = np.concatenate(([0], m))
-
-    Q = np.array([
-        [q[0], - q[1], - q[2], - q[3]],
-        [q[1], + q[0], + q[3], - q[2]],
-        [q[2], - q[3], + q[0], + q[1]],
-        [q[3], + q[2], - q[1], + q[0]]
-    ])
-
-    r = np.dot(Q, v)
-
-    M = np.array([
-        [+m[0], -m[1], -m[2], -m[3]],
-        [+m[1], +m[0], +m[3], -m[2]],
-        [+m[2], -m[3], +m[0], +m[1]],
-        [+m[3], +m[2], -m[1], +m[0]]
-    ])
-
-    p = np.dot(M, r)
-
-    p[0] =  - r[1]*m[0] - r[2]*m[1] - r[3]*m[2]
-    p[1] =  + r[0]*m[0] - r[3]*m[1] + r[2]*m[2]
-    p[2] =  + r[3]*m[0] + r[0]*m[1] - r[1]*m[2]
-    p[3] =  - r[2]*m[0] + r[1]*m[1] + r[0]*m[2]
-
-    u[0] = + p[1]*r[0] - p[0]*r[1] + p[3]*r[2] - p[2]*r[3] + t[0]
-    u[1] = + p[2]*r[0] - p[3]*r[1] - p[0]*r[2] + p[1]*r[3] + t[1]
-    u[2] = + p[3]*r[0] + p[2]*r[1] - p[1]*r[2] - p[0]*r[3] + t[2]
-
-    u[0] = + p[1]*r[0] - p[0]*r[1] + p[3]*r[2] - p[2]*r[3] + t[0]
-    u[1] = + p[2]*r[0] - p[3]*r[1] - p[0]*r[2] + p[1]*r[3] + t[1]
-    u[2] = + p[3]*r[0] + p[2]*r[1] - p[1]*r[2] - p[0]*r[3] + t[2]
-
-    u[0] = - p[0]*r[1] + p[1]*r[0] - p[2]*r[3] + p[3]*r[2] + t[0]
-    u[1] = - p[0]*r[2] + p[1]*r[3] + p[2]*r[0] - p[3]*r[1] + t[1]
-    u[2] = - p[0]*r[3] - p[1]*r[2] + p[2]*r[1] + p[3]*r[0] + t[2]
-
-    return camera_projection(a, u)
-
-
-def calcImgProj(a, q, v, t, m):  # q -> qr0, m -> M
+def projection_(a, q, v, t, m):  # q -> qr0, m -> M
     """
     v : vector part of a unit quaternion that represents a camera rotation
     t : 3D vector which stores a camera position
@@ -115,7 +70,7 @@ def calcImgProj(a, q, v, t, m):  # q -> qr0, m -> M
     return camera_projection(a, u)
 
 
-def calc_image_projection_(a, q, t, m):
+def calc_image_projection_full_rotation(a, q, t, m):
     qr = np.array([
         [-q[1], -q[2], -q[3]],
         [+q[0], -q[3], +q[2]],
