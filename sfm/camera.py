62149e6b6a21f184c068bb068ccbe566d27ac1bf
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-11-21 16:53:02 +0900

    Make the interface of CameraParameters simpler

--- sfm/camera.py
+++ sfm/camera.py
@@ -1,20 +1,27 @@
 import numpy as np
 
 
+def to_tuple_if_scalar(value):
+    if isinstance(value, int) or isinstance(value, float):
+        return (value, value)
+    return value
+
+
 class CameraParameters(object):
-    def __init__(self, fx, fy, s, ox, oy):
-        self.fx = fx
-        self.fx = fx
-        self.fy = fy
-        self.s = s
-        self.ox = ox
-        self.oy = oy
+    def __init__(self, focal_length, offset, skew):
+        self.focal_length = to_tuple_if_scalar(focal_length)
+        self.offset = to_tuple_if_scalar(offset)
+        self.skew = skew
 
     @property
     def matrix(self):
+        ox, oy = self.offset
+        fx, fy = self.focal_length
+        s = self.skew
+
         return np.array([
-            [self.fx, self.s, self.ox],
-            [0, self.fy, self.oy],
+            [fx, s, ox],
+            [0, fy, oy],
             [0, 0, 1]
         ])
 
