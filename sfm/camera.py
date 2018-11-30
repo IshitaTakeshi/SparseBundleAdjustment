85c80a8444de5f81e099e37eddc30c006bf2c4b1
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-11-30 17:20:35 +0900

    Add projection jacobian to the camera model

--- sfm/camera.py
+++ sfm/camera.py
@@ -8,7 +8,7 @@ def to_tuple_if_scalar(value):
 
 
 class CameraParameters(object):
-    def __init__(self, focal_length, offset, skew):
+    def __init__(self, focal_length, offset, skew=0):
         self.focal_length = to_tuple_if_scalar(focal_length)
         self.offset = to_tuple_if_scalar(offset)
         self.skew = skew
@@ -28,3 +28,13 @@ class CameraParameters(object):
     def projection(self, x):
         Z = np.dot(self.matrix, x)
         return Z[0:2] / Z[2]  # TODO make sure Z[2] != 0
+
+    def jacobian(self, point):
+        x, y, z = point
+        fx, fy = self.focal_length
+        return np.array([
+            [fx / z, 0, -fx / pow(z, 2)],
+            [0, fy / z, -fy / pow(z, 2)],
+        ])
+
+
