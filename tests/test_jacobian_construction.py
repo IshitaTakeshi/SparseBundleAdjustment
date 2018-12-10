9ce03b19b82af6b8d5cfe57b365c00c50469027d
IshitaTakeshi <ishitah.takeshi@gmail.com>
2018-12-10 16:14:41 +0900

    Add tests for jacobian construction

--- tests/test_jacobian_construction.py
+++ tests/test_jacobian_construction.py
@@ -9,7 +9,7 @@ from sfm.jacobian import camera_pose as CJ
 from sfm.jacobian import structure as SJ
 
 
-def test_camera_pose_jacobian():
+def test_camera_pose_jacobian_indices():
     # if n_pose_parameters = 4, n_viewpoints = 3, n_3dpoints = 2
     # the row indices should be
     # [0 0 0 0]
@@ -24,7 +24,6 @@ def test_camera_pose_jacobian():
     #          [9 9 9 9]
     #                   [10 10 10 10]
     #                   [11 11 11 11]
-    #
 
     row = np.array([
         0, 0, 0, 0,
@@ -85,7 +84,7 @@ def test_camera_pose_jacobian():
     )
 
 
-def test_structure_jacobian():
+def test_structure_jacobian_indices():
     # if n_point_parameters = 4, n_viewpoints = 3, n_3dpoints = 2
     # the row indices should be
 
@@ -161,5 +160,75 @@ def test_structure_jacobian():
     )
 
 
-test_camera_pose_jacobian()
-test_structure_jacobian()
+def test_camera_pose_jacobian_construction():
+    # n_3dpoints=2, n_viewpoints=3, n_pose_parameters=4
+    jacobians = np.array([
+        [[[6, 5, 8, 6],
+          [4, 0, 3, 0]],
+         [[2, 8, 0, 4],
+          [7, 6, 3, 9]],
+         [[5, 3, 0, 9],
+          [1, 4, 9, 3]]],
+        [[[8, 1, 7, 9],
+          [0, 3, 5, 8]],
+         [[2, 1, 2, 3],
+          [9, 2, 4, 4]],
+         [[8, 7, 7, 7],
+          [3, 7, 5, 1]]]
+    ])
+
+    GT = np.array([
+        [6, 5, 8, 6, 0, 0, 0, 0, 0, 0, 0, 0],
+        [4, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 8, 0, 4, 0, 0, 0, 0],
+        [0, 0, 0, 0, 7, 6, 3, 9, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 5, 3, 0, 9],
+        [0, 0, 0, 0, 0, 0, 0, 0, 1, 4, 9, 3],
+        [8, 1, 7, 9, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 3, 5, 8, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 2, 1, 2, 3, 0, 0, 0, 0],
+        [0, 0, 0, 0, 9, 2, 4, 4, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 8, 7, 7, 7],
+        [0, 0, 0, 0, 0, 0, 0, 0, 3, 7, 5, 1]
+    ])
+
+    assert_array_equal(CJ.camera_pose_jacobian(jacobians).todense(), GT)
+
+
+def test_structure_jacobian_construction():
+    jacobians = np.array([
+        [[[2, 9, 6, 8],
+          [7, 3, 4, 5]],
+         [[3, 5, 2, 7],
+          [5, 0, 7, 7]],
+         [[7, 3, 2, 5],
+          [1, 5, 1, 5]]],
+        [[[7, 6, 3, 6],
+          [1, 9, 4, 1]],
+         [[7, 6, 2, 2],
+          [6, 1, 9, 6]],
+         [[4, 8, 6, 4],
+          [5, 6, 8, 0]]]
+    ])
+
+    GT = np.array([
+        [2, 9, 6, 8, 0, 0, 0, 0],
+        [7, 3, 4, 5, 0, 0, 0, 0],
+        [3, 5, 2, 7, 0, 0, 0, 0],
+        [5, 0, 7, 7, 0, 0, 0, 0],
+        [7, 3, 2, 5, 0, 0, 0, 0],
+        [1, 5, 1, 5, 0, 0, 0, 0],
+        [0, 0, 0, 0, 7, 6, 3, 6],
+        [0, 0, 0, 0, 1, 9, 4, 1],
+        [0, 0, 0, 0, 7, 6, 2, 2],
+        [0, 0, 0, 0, 6, 1, 9, 6],
+        [0, 0, 0, 0, 4, 8, 6, 4],
+        [0, 0, 0, 0, 5, 6, 8, 0]
+    ])
+    assert_array_equal(SJ.structure_jacobian(jacobians).todense(), GT)
+
+
+test_camera_pose_jacobian_indices()
+test_structure_jacobian_indices()
+test_camera_pose_jacobian_construction()
+test_structure_jacobian_construction()
