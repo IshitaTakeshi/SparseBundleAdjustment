e3bd5a6c6fca6fe241dc13b4309635bf24f84ca0
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-02-04 19:05:03 +0900

    Implement the updater based on Schur complement

--- sfm/updaters.py
+++ sfm/updaters.py
@@ -8,7 +8,7 @@
 
 
 import numpy as np
-from scipy.sparse import lil_matrix, diags, linalg
+from scipy.sparse import lil_matrix, diags, linalg, identity
 
 
 # Preconditioned Conjugate Gradients
@@ -55,30 +55,49 @@ class PCGUpdater(object):
 
 
 class LMUpdater(object):
-    def __init__(self, function, jacobian, target):
-        self.f = function
-        self.J = jacobian
-
-        self.mask = self.compute_mask(target)
-        self.masked_x = target[self.mask]
-
-    def compute_mask(self):
-        boolean_mask = np.logical_not(np.isnan(x))
-        # represent the mask as indices
-        return np.arange(J.shape[0])[boolean_mask]
+    def __init__(self, parameter_manager, sba, target):
+        self.manager = parameter_manager
+        self.sba = sba
+        self.target = target
 
     def evaluate_at(self, p):
-        self.p = p
+        A, B = self.sba.jacobian(p)
+        residual = self.target - self.sba.projection(p)
+        self.precomputation(A, B, residual)
 
-        J = self.J(p)
-        f = self.f(p)
+    def precomputation(self, A, B, residual):
+        # it's still ok to write the process below directly to 'evaluate_at'
+        # but separating functions to make tests easier
 
-        f, J = f[self.mask], J[self.mask, :]
+        self.A = A
+        self.B = B
+        self.U = A.T.dot(A)
+        self.V = B.T.dot(B)
+        self.W = A.T.dot(B)
 
-        self.K = J.T.dot(J)
-        self.D = diags(self.K.diagonal())
-        self.g = J.T.dot(self.masked_x - f)
+        self.epsilon_a = A.T.dot(residual)
+        self.epsilon_b = B.T.dot(residual)
 
-    def calculate(self, lambda_):
-        dp = linalg.spsolve(self.K + lambda_ * self.D, self.g)
-        return self.p + dp
+    def calc_update(self, lambda_):
+        A, B = self.A, self.B
+        W = self.W
+        epsilon_a, epsilon_b = self.epsilon_a, self.epsilon_b
+
+        # Actually U and V below are U_star and V_star respectively,
+        # although beautiful is better than ugly
+        U = self.U + lambda_ * identity(self.manager.length_all_poses)
+        V = self.V + lambda_ * identity(self.manager.length_all_3dpoints)
+
+        V_inv = linalg.inv(V)
+
+        S = U - W.dot(V_inv).dot(W.T)
+        b = epsilon_a - W.dot(V_inv).dot(epsilon_b)
+        delta_a = linalg.spsolve(S, b)
+
+        b = epsilon_b - W.T.dot(delta_a)
+        delta_b = linalg.spsolve(V, b)
+
+        # in this case it seems spsolve returns np.ndarray
+        # so we can just concatenate directly
+        dp = np.concatenate((delta_a, delta_b))
+        return dp
