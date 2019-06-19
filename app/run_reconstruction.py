6ff77645c3756fb04fe09d0ec5fc33a5498f526c
IshitaTakeshi <ishitah.takeshi@gmail.com>
2019-06-19 15:30:47 +0900

    Simplify the runner

--- app/run_reconstruction.py
+++ app/run_reconstruction.py
@@ -4,8 +4,6 @@ import numpy as np
 
 sys.path.append(str(Path(__file__).resolve().parent.parent))
 
-from sfm.sba import SBA
-from sfm.lm import LevenbergMarquardt
 from sfm.camera import CameraParameters
 
 from dataset.sba import load_sba
@@ -29,21 +27,12 @@ def approximate_camera_parameters(image_shape):
 
 def main():
     camera_parameters = CameraParameters(focal_length=1, offset=0)
-    # observations = load_npy_observation(filename)
-    ground_truth, observations = load_sba("./data/54pts.txt")
+    observations = load_npy_observation(sys.argv[1])
 
-    n_3dpoints, n_viewpoints = observations.shape[:2]
+    # points3d, poses = optimize_scipy(sba, observations)
+    points3d, poses = optimize_lm(camera_parameters, observations)
 
-    print("n_3dpoints: ", n_3dpoints)
-    print("n_viewpoints: ", n_viewpoints)
-
-    sba = SBA(camera_parameters, n_3dpoints, n_viewpoints)
-
-    points3d, poses = optimize_scipy(sba, observations)
-    # points3d, poses = optimize_lm(sba, observation)
-
-    print("points3d.shape", points3d.shape)
-    print("poses.shape", poses.shape)
+    np.save("points3d.npy", points3d)
     plot_reconstructed(points3d)
 
 
